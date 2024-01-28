2aa6eea4709ad5a923fd64d3902fc77157f3d822
Raza478 <muhraza191@gmail.com>
2024-01-29 00:43:47 +0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from '../api/routes/user.route.js'
+import authRoutes from '../api/routes/auth.route.js'
 
 
 dotenv.config();
-const app = express()
+const app = express();
+app.use(express.json());
 mongoose
     .connect(process.env.MONGO)
     .then(()=>{
@@ -19,3 +21,4 @@ app.listen(3000, ()=>{
 })
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
